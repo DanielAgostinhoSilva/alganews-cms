efe4aae46a479213e255253fb9955b3526ea1bce
Daniel <daniel.agostinho@hotmail.com>
2022-09-27 20:23:17 -0300

    feat: 4.6. Criando o boilerplate do Botão

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,6 +1,6 @@
 import {ComponentStory, ComponentMeta} from '@storybook/react';
 
-import {Button} from './Button';
+import Button, {ButtonProps} from '../components/Button/Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
