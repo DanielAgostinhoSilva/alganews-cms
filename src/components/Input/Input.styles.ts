501c756d8db77953d2a04d5d5a51e96a5e68311d
Daniel <daniel.agostinho@hotmail.com>
2022-10-02 13:41:42 -0300

    feat: 4.11. Estilizando o componente Input

--- src/components/Input/Input.styles.ts
+++ src/components/Input/Input.styles.ts
@@ -1,7 +1,34 @@
 import styled from "styled-components";
+import {transparentize} from "polished";
 
 export const Wrapper = styled.label`
   display: flex;
   gap: 16px;
   flex-direction: column;
+  
+  span.Label {
+    font-size: 14px;
+    font-weight: 500;
+    color: #274060;
+  }
+  
+  input {
+    height: 28px;
+    font-size: 18px;
+    font-weight: 500;
+    
+    box-sizing: border-box;
+    padding-bottom: 6px;
+    border: none;
+    outline: none;
+    
+    border-radius: 0;
+    border-bottom: 1px solid #274060;
+    
+    &::placeholder {
+      color: ${transparentize(0.5, '#274060')};
+    }
+    
+    color: #274060;
+  }
 `
