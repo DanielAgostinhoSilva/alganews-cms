2c0ebd58b758ffc495134b0008bacbe72ac000e7
Daniel <daniel.agostinho@hotmail.com>
2022-10-08 10:49:55 -0300

    feat: 4.28. Apresentando e instalando o ChartJS

---
+++ src/components/Chart/Chart.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+export const options = {
+    responsive: true,
+    interaction: {
+        mode: 'index' as const,
+        intersect: false,
+    },
+    stacked: false,
+    plugins: {
+        title: {
+            display: true,
+            text: 'Chart.js Line Chart - Multi Axis',
+        },
+    },
+    scales: {
+        y: {
+            type: 'linear' as const,
+            display: true,
+            position: 'left' as const,
+        },
+        y1: {
+            type: 'linear' as const,
+            display: true,
+            position: 'right' as const,
+            grid: {
+                drawOnChartArea: false,
+            },
+        },
+    },
+};
+
+
+const data = {
+    labels: ['1', '2', '3', '4', '5', '6'],
+    datasets: [
+        {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            fill: false,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgba(255, 99, 132, 0.2)',
+            yAxisID: 'y-axis-1',
+        },
+        {
+            label: '# of No Votes',
+            data: [1, 2, 1, 1, 2, 2],
+            fill: false,
+            backgroundColor: 'rgb(54, 162, 235)',
+            borderColor: 'rgba(54, 162, 235, 0.2)',
+            yAxisID: 'y-axis-2',
+        },
+    ],
+};
+
+export interface ChartProps {}
+
+export default function Chart () {
+    return <div>
+        <Line
+            data={data}
+            options={options}
+        />
+    </div>
+}
