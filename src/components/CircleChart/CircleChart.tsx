5414a8cfa84606a99a22bfff18aacfd074995c67
Daniel <daniel.agostinho@hotmail.com>
2022-10-11 19:13:24 -0300

    feat: 4.43. Setup do CircleChart

--- src/components/CircleChart/CircleChart.tsx
+++ src/components/CircleChart/CircleChart.tsx
@@ -1,4 +1,5 @@
 import * as CC from './CircleChart.styles'
+import {useEffect, useState} from "react";
 
 export interface CircleChartProps {
     size: number
@@ -9,6 +10,24 @@ export interface CircleChartProps {
 }
 
 function CircleChart (props: CircleChartProps) {
+
+    const getThemeColor = () => props.theme === 'primary' ? '#09f' : '#274060'
+
+    const THEME = getThemeColor();
+    const STROKE_WIDTH = props.strokeWidth || 8
+    const STROKE_COLOR = THEME
+
+    const CENTER = props.size / 2
+    const RADIUS = props.size / 2 - STROKE_WIDTH / 2
+    const CIRCUMFERENCE = 2 * Math.PI * RADIUS
+
+    const [offset, setOffeset] = useState(CIRCUMFERENCE)
+    
+    useEffect(() => {
+        const progressOffset = ((100 - props.progress) / 100) * CIRCUMFERENCE
+        setOffeset(progressOffset)
+    },[setOffeset, CIRCUMFERENCE, props.progress, offset])
+
     return <CC.Wrapper>
         todo: circle chart
     </CC.Wrapper>
