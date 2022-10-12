d20af43ef60bb196ac4aee80e0b047156fce83ca
Daniel <daniel.agostinho@hotmail.com>
2022-10-12 18:28:34 -0300

    feat: 4.51. Estilizando o TagInput

--- src/stories/TagInput.stories.tsx
+++ src/stories/TagInput.stories.tsx
@@ -10,7 +10,25 @@ const Template: Story<TagInputProps> = (args) => <TagInput {...args} />;
 
 export const Default = Template.bind({})
 Default.args = {
-    tags: [{id: '1', text: 'Java'}],
+    tags: [
+        {id: '1', text: 'Java'}
+    ],
     placeholder: 'Insira as tags deste post'
 }
+
+export const VariousTags = Template.bind({})
+VariousTags.args = {
+    tags: [
+        {id: '1', text: 'Java'},
+        {id: '2', text: 'GO'},
+        {id: '3', text: 'C++'},
+        {id: '4', text: 'C#'},
+        {id: '5', text: '.Net'},
+        {id: '6', text: 'HTML'},
+        {id: '7', text: 'CSS'},
+        {id: '8', text: 'Javascript'},
+    ],
+    placeholder: 'Insira as tags deste post'
+}
+
 
