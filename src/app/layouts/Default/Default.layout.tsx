9b304c1ea7b1f7349bd92e61eedf16307459b174
Daniel <daniel.agostinho@hotmail.com>
2022-10-15 17:50:07 -0300

    feat: 6.18. Transformando um método em uma macrotask (event loop)

--- src/app/layouts/Default/Default.layout.tsx
+++ src/app/layouts/Default/Default.layout.tsx
@@ -3,9 +3,17 @@ import Navbar from "../../components/NavBar/Navbar";
 import Logo from "../../components/Logo/Logo";
 import SessionController from "../../components/SessionController";
 import confirm from "../../../core/utils/confirm";
+import info from "../../../core/utils/info";
 
 interface DefaultLayoutProps {
     children: React.ReactNode
+}
+
+function showModalInfo() {
+    return () => info({
+        title: 'Você foi deslogado',
+        description: 'Você será redirecionado para a página de login'
+    })
 }
 
 function DefaultLayout(props: DefaultLayoutProps) {
@@ -25,7 +33,10 @@ function DefaultLayout(props: DefaultLayoutProps) {
                     name={'Daniel Silva'}
                     description={'editor há 2 anos'}
                     onLogout={() => {
-                        confirm({title: 'Você quer deslogar?'})
+                        confirm({
+                            title: 'Você quer deslogar?',
+                            onConfirm: showModalInfo()
+                        })
                     }}
                 />
             </DL.Aside>
