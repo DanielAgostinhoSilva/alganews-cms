b38bfde96252f3bdb7ec7af957a91350a88e20d8
Daniel <daniel.agostinho@hotmail.com>
2022-10-15 10:01:51 -0300

    feat: 6.12. Feature EditorsList

--- src/app/views/EditorsList.view.tsx
+++ src/app/views/EditorsList.view.tsx
@@ -1,9 +1,10 @@
 import DefaultLayout from "../layouts/Default";
 import usePageTitle from "../../core/hooks/usePageTitle";
+import EditorsList from "../features/EditorsList";
 
 export default function EditorsListView() {
     usePageTitle('Lista de editores')
     return <DefaultLayout>
-        todo: editors views
+        <EditorsList />
     </DefaultLayout>
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,7 @@ import './core/imports.css';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomeView from "./app/views/Home.view";
-import Contact from "./app/views/Contact.view";
 import NotFound404 from "./app/views/NotFound404.view";
-import UserVeiw from "./app/views/User.veiw";
 import GlobalStyles from "./core/globalStyles";
 import EditorsListView from "./app/views/EditorsList.view";
 
