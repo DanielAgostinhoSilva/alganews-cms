b86b3a447369e80ed510c32318180d8a364cbd69
Daniel <daniel.agostinho@hotmail.com>
2022-10-15 16:17:49 -0300

    feat: 6.15. Página 404

--- src/app/views/NotFound404.view.tsx
+++ src/app/views/NotFound404.view.tsx
@@ -1,5 +1,36 @@
+import notFound from '../../assets/NotFound.svg'
+import Button from "../components/Button/Button";
+import styled from "styled-components";
+import {useNavigate} from "react-router-dom";
+
 export default function NotFound404() {
-    return <div>
-        <h1>Pagina não encontrada</h1>
-    </div>
+    const navigate = useNavigate();
+    return <NofFound404Wrapper>
+        <span>Oops!</span>
+        <h1>Não encontramos esta página</h1>
+        <img src={notFound} alt="Não encontrado"/>
+        <Button
+            variant={'primary'}
+            label={'Ir para a home'}
+            onClick={() => navigate("/", {replace: true})}
+        />
+    </NofFound404Wrapper>
 }
+
+const NofFound404Wrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 32px;
+  
+  span {
+    font-size: 72px;
+  }
+  
+  h1 {
+    font-size: 18px;
+    font-weight: 400;
+  }
+`
