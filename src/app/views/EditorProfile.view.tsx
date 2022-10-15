757dff1cf82203d447e6a9683118bf8cab5e9b87
Daniel <daniel.agostinho@hotmail.com>
2022-10-15 18:23:53 -0300

    feat: 6.19. Estilizando um componente de uma biblioteca externa

---
+++ src/app/views/EditorProfile.view.tsx
@@ -0,0 +1,9 @@
+import DefaultLayout from "../layouts/Default";
+
+function EditorProfileView() {
+    return <DefaultLayout>
+        todo: editor profile fiew
+    </DefaultLayout>
+}
+
+export default EditorProfileView
