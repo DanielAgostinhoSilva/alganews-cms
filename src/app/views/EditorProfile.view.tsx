1ffd79628f26d78e069f23ba0a4bda985b751b8c
Daniel <daniel.agostinho@hotmail.com>
2022-10-16 17:55:27 -0300

    feat: 6.22. Blindando a view com ErrorBoundary

--- src/app/views/EditorProfile.view.tsx
+++ src/app/views/EditorProfile.view.tsx
@@ -1,9 +1,12 @@
 import DefaultLayout from "../layouts/Default";
 import EditorProfile from "../features/EditorProfile";
+import ErrorBoundary from "../components/ErrorBoundary/ErrorBoundary";
 
 function EditorProfileView() {
     return <DefaultLayout>
-        <EditorProfile hidePersonalData={true}/>
+        <ErrorBoundary>
+            <EditorProfile hidePersonalData={true}/>
+        </ErrorBoundary>
     </DefaultLayout>
 }
 
