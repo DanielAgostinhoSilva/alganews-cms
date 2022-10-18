01058869dd39729132b1abfb919a424f8c11531f
Daniel <daniel.agostinho@hotmail.com>
2022-10-18 18:02:15 -0300

    feat: 8.16. Lidando com paramentros da API

--- src/app/features/PostForm.tsx
+++ src/app/features/PostForm.tsx
@@ -9,22 +9,35 @@ import WordPriceCounter from "../components/WordPriceCounter";
 import Button from "../components/Button/Button";
 import countWordsInMarkdown from "../../core/utils/countWordsInMarkdown";
 import info from "../../core/utils/info";
+import PostService from "../../sdk/services/Post.service";
 
 export default function PostForm() {
     const [tags, setTags] = useState<Tag[]>([])
     const [body, setBody] = useState('')
+    const [title, setTitle] = useState('')
 
-    function handleFormSubit(event: React.FormEvent<HTMLFormElement>) {
+    async function handleFormSubit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault()
+        const newPost = {
+            body,
+            title,
+            tags: tags.map(tag => tag.text),
+            imageUrl: ''
+        }
+
+        const insertedPost = await PostService.insertNewPost(newPost)
+
         info({
             title: 'Post salvo com sucesso',
-            description: 'Você acabou de salver o post'
+            description: `Você acabou de criar o post com o id ${insertedPost.id}`
         })
     }
 
     return <PostFormWrapper onSubmit={handleFormSubit}>
         <Input
             label={'título'}
+            value={title}
+            onChange={e => setTitle(e.currentTarget.value)}
             placeholder={'e.g.: Como fiquei rico apredendo React'}
         />
         <ImageUpload label={'Thumbnail do post'} />
