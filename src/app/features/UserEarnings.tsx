7a583f890f412a48e4b6e0230dfdff06078cd4e0
Daniel <daniel.agostinho@hotmail.com>
2022-10-22 20:51:29 -0300

    feat: 8.27. Ganhos do usuário

--- src/app/features/UserEarnings.tsx
+++ src/app/features/UserEarnings.tsx
@@ -1,12 +1,26 @@
 import styled from "styled-components";
 import ValueDescriptor from "../components/ValueDescriptor/ValueDescriptor";
+import {useEffect, useState} from "react";
+import {User} from "../../sdk/@types";
+import UserService from "../../sdk/services/User.service";
 
 export default function UserEarnings() {
+    const [user, setUser] = useState<User.Detailed>()
+
+    useEffect(() => {
+        UserService
+            .getDeatiledUser(7)
+            .then(setUser)
+    }, [])
+
+    if(!user)
+        return null
+
     return <UserEarningsWrapper>
-        <ValueDescriptor description={'ganhos no mês'} value={560322.32} color={'primary'} isCurrency={true}/>
-        <ValueDescriptor description={'ganhos na semana'} value={560322.32} color={'primary'} isCurrency={true}/>
-        <ValueDescriptor description={'ganhos no sempre'} value={560322.32} color={'default'} isCurrency={true}/>
-        <ValueDescriptor description={'total de palavras'} value={2_345_347} color={'default'} isCurrency={false}/>
+        <ValueDescriptor description={'ganhos no mês'} value={user.metrics.monthlyEarnings} color={'primary'} isCurrency={true}/>
+        <ValueDescriptor description={'ganhos na semana'} value={user.metrics.weeklyEarnings} color={'primary'} isCurrency={true}/>
+        <ValueDescriptor description={'ganhos no sempre'} value={user.metrics.lifetimeEarnings} color={'default'} isCurrency={true}/>
+        <ValueDescriptor description={'total de palavras'} value={user.metrics.lifetimeWords} color={'default'} isCurrency={false}/>
     </UserEarningsWrapper>
 }
 
