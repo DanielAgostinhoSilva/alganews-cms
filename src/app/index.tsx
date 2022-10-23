2517a800ce0fae373f1282ce9132503cd635131e
Daniel <daniel.agostinho@hotmail.com>
2022-10-23 17:43:24 -0300

    feat: 8.39. Prevenindo erros de passarem em branco com onUnhandledRejection
    https://app.algaworks.com/aulas/3865/prevenindo-erros-de-passarem-em-branco-com-onunhandledrejection

---
+++ src/app/index.tsx
@@ -0,0 +1,29 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomeView from "./views/Home.view";
+import EditorsListView from "./views/EditorsList.view";
+import EditorProfileView from "./views/EditorProfile.view";
+import PostCreateView from "./views/PostCreate.view";
+import NotFound404 from "./views/NotFound404.view";
+import React, {useEffect} from "react";
+import info from "../core/utils/info";
+
+export default function App() {
+    useEffect(() => {
+        window.onunhandledrejection = function (error: PromiseRejectionEvent) {
+            info({
+                title: error.reason.response?.data.title || 'Erro',
+                description: error.reason.response?.data.detail || error.reason.message
+            })
+        }
+    },[])
+
+    return <BrowserRouter>
+        <Routes>
+            <Route path={'/'} element={<HomeView/>}/>
+            <Route path={'/editores'} element={<EditorsListView/>}/>
+            <Route path={'/editores/:id'} element={<EditorProfileView/>}/>
+            <Route path={'/posts/criar'} element={<PostCreateView/>}/>
+            <Route path={'*'} element={<NotFound404/>}/>
+        </Routes>
+    </BrowserRouter>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,25 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './core/imports.css';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import HomeView from "./app/views/Home.view";
-import NotFound404 from "./app/views/NotFound404.view";
 import GlobalStyles from "./core/globalStyles";
-import EditorsListView from "./app/views/EditorsList.view";
-import PostCreateView from "./app/views/PostCreate.view";
-import EditorProfileView from "./app/views/EditorProfile.view";
+import App from "./app";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path={'/'} element={<HomeView/>}/>
-                <Route path={'/editores'} element={<EditorsListView/>}/>
-                <Route path={'/editores/:id'} element={<EditorProfileView/>}/>
-                <Route path={'/posts/criar'} element={<PostCreateView/>}/>
-                <Route path={'*'} element={<NotFound404/>}/>
-            </Routes>
-        </BrowserRouter>
+        <App />
         <GlobalStyles/>
     </React.StrictMode>,
     document.getElementById('root')
