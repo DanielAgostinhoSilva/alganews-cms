55ba914146a304613548df2d370fd99fe5732fcd
Daniel <daniel.agostinho@hotmail.com>
2022-10-23 17:23:46 -0300

    feat: 8.38. Desafio - Loading animado com blur
    https://app.algaworks.com/aulas/3864/desafio-loading-animado-com-blur

--- src/app/components/Loading/Loading.styles.ts
+++ src/app/components/Loading/Loading.styles.ts
@@ -7,12 +7,26 @@ export const LoadingWrapper = styled.div`
   width: 100%;
   height: 100%;
 
-  background-color: rgba(0,0,0,.7);
+  background-color: rgba(0, 0, 0, .7);
   z-index: 99;
 
   display: flex;
   justify-content: center;
   align-items: center;
+  backdrop-filter: blur(5px);
+
+  animation: .25s pop ease;
+
+  @keyframes pop {
+    from {
+      background-color: rgba(0, 0, 0, .0);
+      opacity: 0;
+    }
+    to {
+      background-color: rgba(0, 0, 0, .7);
+      opacity: 1;
+    }
+  }
 
 
   .lds-ring {
@@ -21,6 +35,7 @@ export const LoadingWrapper = styled.div`
     width: 80px;
     height: 80px;
   }
+
   .lds-ring div {
     box-sizing: border-box;
     display: block;
@@ -33,15 +48,19 @@ export const LoadingWrapper = styled.div`
     animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
     border-color: #09f transparent transparent transparent;
   }
+
   .lds-ring div:nth-child(1) {
     animation-delay: -0.45s;
   }
+
   .lds-ring div:nth-child(2) {
     animation-delay: -0.3s;
   }
+
   .lds-ring div:nth-child(3) {
     animation-delay: -0.15s;
   }
+
   @keyframes lds-ring {
     0% {
       transform: rotate(0deg);
